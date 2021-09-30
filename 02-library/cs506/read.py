a766b48896b945e3cfcbf0d8bbeb4cf29364bc4c
Cathylixi <90333859+Cathylixi@users.noreply.github.com>
2021-09-30 10:27:40 -0400

    Update read.py

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,4 +3,6 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    one_file = open('csv_file_path','r')
+    new_list = list(csv.reader(one_file, delimiter=";"))
+    return new_list
